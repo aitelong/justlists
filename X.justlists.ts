c31b0aea126c540817cfc0e3195df25e64913b4f
X联盟-X Alliance-艾特龙 <1662282367@qq.com>
2024-09-30 00:25:10 +0800

    更新 X.justlists.ts

--- X.justlists.ts
+++ X.justlists.ts
@@ -1,3 +1,4 @@
+<pre style="caret-color: rgb(0, 0, 0); color: rgb(0, 0, 0); font-style: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; widows: auto; word-spacing: 0px; -webkit-tap-highlight-color: rgba(26, 26, 26, 0.3); -webkit-text-size-adjust: auto; -webkit-text-stroke-width: 0px; text-decoration: none; overflow-wrap: break-word; white-space: pre-wrap;">
 {
    "name": "X",
    "logoURI": "https://raw.githubusercontent.com/aitelong/justlists/main/IMG_1271.jpeg",
