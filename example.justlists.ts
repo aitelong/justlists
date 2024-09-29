6ce1210faa078760aa1aa53a86a3c229d5ac6aea
X联盟-X Alliance-艾特龙 <1662282367@qq.com>
2024-09-30 06:38:41 +0800

    更新 example.justlists.ts

--- example.justlists.ts
+++ example.justlists.ts
@@ -1,27 +1,36 @@
 {
-   "name": "X",
-   "logoURI": "https://github.com/aitelong/X/blob/main/X.jpg",
-   "timestamp": 1724617097820,
-   "tokens": [{
-           "chainId": 1,
-           "address": "TVWwjAYjSNrNBK7q62pgkNz6ahSyDXVTs8",
-           "name": "X",
-           "symbol": "X",
-           "decimals": 8,
-           "logoURI": "https://github.com/aitelong/X/blob/main/X.jpg"
-       },
-       {
-           "chainId": 1,
-           "address": "TVWwjAYjSNrNBK7q62pgkNz6ahSyDXVTs8",
-           "name": "X",
-           "symbol": "X",
-           "decimals": 18,
-           "logoURI": "https://github.com/aitelong/X/blob/main/X.jpg"
-       }
-   ],
-   "version": {
-       "major": 0,
-       "minor": 1,
-       "patch": 0
-   }
+    "name":"X",
+    "tokens":[
+        {
+            "symbol":"WTRX",
+            "address":"TNUC9Qb1rRpS5CbWLmNMxXBjyFoydXjWFR",
+            "chainId":1,
+            "decimals":6,
+            "name":"Wrapped TRX",
+            "logoURI":"https://coin.top/production/upload/logo/TNUC9Qb1rRpS5CbWLmNMxXBjyFoydXjWFR.png"
+        },
+        {
+            "symbol":"USDT",
+            "address":"TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+            "chainId":1,
+            "decimals":6,
+            "name":"Tether USD",
+            "logoURI":"https://coin.top/production/logo/usdtlogo.png"
+        },
+        {
+            "symbol":"X",
+            "address":"TVWwjAYjSNrNBK7q62pgkNz6ahSyDXVTs8",
+            "chainId":1,
+            "decimals":16,
+            "name":"X",
+            "logoURI":"https://raw.githubusercontent.com/aitelong/justlists/main/IMG_1271.jpeg"
+        }
+    ],
+    "logoURI":"https://raw.githubusercontent.com/aitelong/justlists/main/IMG_1271.jpeg",
+    "version":{
+        "patch":0,
+        "major":1,
+        "minor":1
+    },
+    "timestamp":1727538985000
 }
