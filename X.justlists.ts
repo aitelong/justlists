96cb1b0757991059e4c4265b1b1a51a39d9e2731
X联盟-X Alliance-艾特龙 <1662282367@qq.com>
2024-09-30 00:40:49 +0800

    更新 X.justlists.ts

--- X.justlists.ts
+++ X.justlists.ts
@@ -1,36 +1,26 @@
 {
-    "name":"Fox",
-    "tokens":[
-        {
-            "symbol":"WTRX",
-            "address":"TNUC9Qb1rRpS5CbWLmNMxXBjyFoydXjWFR",
-            "chainId":1,
-            "decimals":6,
-            "name":"Wrapped TRX",
-            "logoURI":"https://coin.top/production/upload/logo/TNUC9Qb1rRpS5CbWLmNMxXBjyFoydXjWFR.png"
-        },
-        {
-            "symbol":"USDT",
-            "address":"TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
-            "chainId":1,
-            "decimals":6,
-            "name":"Tether USD",
-            "logoURI":"https://coin.top/production/logo/usdtlogo.png"
-        },
-        {
-            "symbol":"FOX",
-            "address":"TEgxhyHLeqZitwBXHgw82pWyy4Sc8oMcL3",
-            "chainId":1,
-            "decimals":6,
-            "name":"FoxCoin",
-            "logoURI":"https://static.tronscan.org/production/upload/logo/new/TEgxhyHLeqZitwBXHgw82pWyy4Sc8oMcL3.png"
-        }
-    ],
-    "logoURI":"https://static.tronscan.org/production/upload/logo/new/TEgxhyHLeqZitwBXHgw82pWyy4Sc8oMcL3.png",
-    "version":{
-        "patch":0,
-        "major":1,
-        "minor":1
-    },
-    "timestamp":1727538985000
-}
+   "name": "X",
+   "logoURI": "https://raw.githubusercontent.com/aitelong/justlists/main/IMG_1271.jpeg", 
+   "tokens": [{
+           "chainId": 1,
+           "address": "TVWwjAYjSNrNBK7q62pgkNz6ahSyDXVTs8", 
+           "name": "X", 
+           "symbol": "X", 
+           "decimals": 18,
+           "logoURI": "https://raw.githubusercontent.com/aitelong/justlists/main/IMG_1271.jpeg" 
+       },
+       {
+           "chainId": 1,
+           "address": "TVWwjAYjSNrNBK7q62pgkNz6ahSyDXVTs8",
+           "name": "X",
+           "symbol": "X",
+           "decimals": 18,
+           "logoURI": "https://raw.githubusercontent.com/aitelong/justlists/main/IMG_1271.jpeg"
+       }
+   ],
+   "version": { 
+       "major": 1, 
+       "minor": 0, 
+       "patch": 0  
+   }
+}"timestamp": 1610339529000,
