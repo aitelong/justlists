16cfe6246d6990d420e28614288156b36de1e8e9
X联盟-X Alliance-艾特龙 <1662282367@qq.com>
2024-09-30 01:43:49 +0800

    更新 X.justlists.ts

--- X.justlists.ts
+++ X.justlists.ts
@@ -1,21 +1,36 @@
 {
     "name":"X",
     "tokens":[
-         {
+        {
+            "symbol":"WTRX",
+            "address":"TNUC9Qb1rRpS5CbWLmNMxXBjyFoydXjWFR",
+            "chainId":1,
+            "decimals":6,
+            "name":"Wrapped TRX",
+            "logoURI":"https://coin.top/production/upload/logo/TNUC9Qb1rRpS5CbWLmNMxXBjyFoydXjWFR.png"
+        },
+        {
+            "symbol":"USDT",
+            "address":"TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+            "chainId":1,
+            "decimals":6,
+            "name":"Tether USD",
+            "logoURI":"https://coin.top/production/logo/usdtlogo.png"
+        },
+        {
             "symbol":"X",
-
             "address":"TVWwjAYjSNrNBK7q62pgkNz6ahSyDXVTs8",
             "chainId":1,
-            "decimals":18,
+            "decimals":16,
             "name":"X",
             "logoURI":"https://raw.githubusercontent.com/aitelong/justlists/main/IMG_1271.jpeg"
         }
-   ],
-    "logoURI":"https://raw.githubusercontent.com/aitelong/justlists/main/IMG_1271.jpeg",
+    ],
+    "logoURI":"https://raw.githubusercontent.com/aitelong/justlists/main/IMG_1271.jpeg.png",
     "version":{
-        "patch":1,
+        "patch":0,
         "major":1,
-        "minor":0
+        "minor":1
     },
     "timestamp":1727538985000
 }
